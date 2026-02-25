36a57df76054558587982d1778b3a1178663db78
Joshua McAdam <st10506918@myemeris.edu.za>
2026-02-25 09:46:00 +0200

    My First Time Working with GUI

---
+++ src/main/java/com/mycompany/firstgui/FirstGui.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstgui;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author lab_services_student
+ */
+public class FirstGui {
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Dip Dip Potatoe Chip!", "Random KAK Message", 2);
+        
+        JOptionPane.showMessageDialog(null, "Programing is fun!!!", "Program", 1);
+        
+        JOptionPane.showMessageDialog(null, "VW drivers are cautious, but they drive like taxi drivers!!", "VW Drivers", 2);
+        
+        JOptionPane.showMessageDialog(null, "Dividing any number by zero is not allowed !!", "Dividing by Zero", 0);
+        
+        
+    }
+}
